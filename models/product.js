e3e9ebcec1074551faa65ced9bda68bda2fca8f5
Fareya Azam <113286085+fareya22@users.noreply.github.com>
2024-05-05 00:38:15 +0600

    Add files via upload

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+       productName: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
